ad3f2edc307b1dd07258b8103b20509b3910d4bd
alex <alexgre@ufl.edu>
2017-12-17 01:49:43 -0500

    finish load in dict function

--- src/edu/ufl/biotext2kb/extraction/extractentityandpredicate/ExtractEntityAndPredicate.java
+++ src/edu/ufl/biotext2kb/extraction/extractentityandpredicate/ExtractEntityAndPredicate.java
@@ -1,59 +1,152 @@
 package edu.ufl.biotext2kb.extraction.extractentityandpredicate;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.inject.AbstractModule;
+import edu.ufl.biotext2kb.BioText2KBUtils;
+import joinery.DataFrame;
+import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class ExtractEntityAndPredicate extends AbstractModule {
-    String entityFileName = null;
-    String predicateFileName = null;
-    HashSet<String> alSentences = null;
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ExtractEntityAndPredicate.class);
 
-    /**
-     * empty constructor
-     */
-    public ExtractEntityAndPredicate(){
+    private ImmutableSet<String> sents;
+    private ImmutableSortedSet<String> entitiesSet;
+    private ImmutableSortedSet<String> predicateSet;
 
+    //TODO replace the stop-words below with  with dynamic detection implementation
+    //*********************************************************************************************************************************************
+    static HashSet<String> stop_words_entities_ = new HashSet(Arrays.asList("normal","intake","intakes","known",
+            "preventing","has","have","influences","influence","identified","decreases","associated","associated with","not","acts","act",
+            "decrease","high","low","lower","higher","other","on","affect","affects","i","ii","iii","is","a","46","24","use","uses"
+            ,"targeting","or","25","be","part","parts","pre","light","novel","none","all","total","with","without","reported","half","benefits","benefit","changes","change",
+            "new","be","are","as","at","and","this","well","may","increase","share","related","high","associated","links","play","plays","help","helps"));
+    static HashSet<String> stop_words_predicates_ = new HashSet(Arrays.asList("under","now","well","while","ai","ua","'s","-"));
+    //*********************************************************************************************************************************************
+
+    public ExtractEntityAndPredicate(String entityFileName, String predicatefileName){
+        this.entitiesSet = loadEntityOrPredicateSet(entityFileName, stop_words_entities_);
+        this.predicateSet = loadEntityOrPredicateSet(predicatefileName, stop_words_predicates_);
     }
 
-    /**
-     * constructor taking entity and predicate dictionary files
-     * @param entFileName
-     * @param predFileName
-     */
-    public ExtractEntityAndPredicate(String entFileName, String predFileName){
-        this.entityFileName = entFileName;
-        this.predicateFileName = predFileName;
-    }
 
     /**
-     *
-     * @param line
-     * @return
+     * The function will scan every sentence from the input and extract entities and predicates in each of them based on pre-defined dictionaries
+     * The results are stored as a Map structure which can be easy to handle as a json
+     * The key is the original sentence
+     * The value is a list of all the triples of entities and predicates extracted from the key sentence
+     * @param sents a set of sentences obtained from abstracts of PreMed publications
+     * @return a Immutable map with entities-predicates pairs extracted from each sentences
      */
-    public String searchEntityOrPredicate(String line){
+    public ImmutableMap<String, ArrayList<HashSet<String>>> entityPredicatePairsExtraction(ImmutableSet<String> sents){
+        this.sents = sents;
+
+        ImmutableMap.Builder<String, ArrayList<HashSet<String>>> entityPredicatePairs = ImmutableMap.builder();
+
+        for(String eachSent: sents){
+            ArrayList<HashSet<String>> entityAndPredicateInOneSent = new ArrayList();
+            //extract the triple (entity, predicate, another entity)
+
+            //add each triple to a list
+
+            //add the list of triples as value into hashmap with key as the sentence where these triples are extracted
+            entityPredicatePairs.put(eachSent, entityAndPredicateInOneSent);
+        }
+
+        return entityPredicatePairs.build();
+    }
+
+    private String matchEntityOrPredicateInSentences(String line){
 
         return null;
     }
 
-    public
 
-    public void setEntityFileName(String entityFileName) {
-        this.entityFileName = entityFileName;
+    /**
+     * the function aims to read-in the predefined entities and predicates to help extract these from the sentences generated from pre-processing
+     * @param fileName entity or predicate pre-extracted dictionary
+     * @return a set of entities or predicates
+     */
+    private ImmutableSortedSet<String> loadEntityOrPredicateSet(String fileName, HashSet<String> stopWords){
+        //sort all entities or predicates based on their length
+        SortedSet<String> entityOrPredicateDictBuilder = new TreeSet(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if(s1 == s2) return 0;
+                //use word length to sort
+                if(s1.length() > s2.length()) return 1;
+                else return -1;
+            }
+        });
+
+        BufferedReader br = null;
+        String word;
+
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line;
+            while((line = br.readLine()) != null){
+                 word = line.split(",")[0];
+                //remove the entity or predicate word in stop words list since we do not want to include them
+                if(!stopWords.contains(word)) {
+                    entityOrPredicateDictBuilder.add(word);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            BioText2KBUtils.closeStream(br);
+        }
+
+        System.out.println(entityOrPredicateDictBuilder);
+
+        //return as immutableSet
+        return ImmutableSortedSet.copyOfSorted(entityOrPredicateDictBuilder);
     }
 
-    public void setPredicateFileName(String predicateFileName) {
-        this.predicateFileName = predicateFileName;
+
+    /**
+     * The function aims to read in the data from csv file as a dataframe
+     * @param fileName the csv file need to be loaded in
+     * @return dataframe created by reading in a csv file
+     */
+    public DataFrame<Object> loadEntityOrPredicateDataFrame(String fileName){
+        //this is df is equivalent to dataframe from pandas in python
+        DataFrame<Object> df = null;
+
+        try {
+            df = DataFrame.readCsv(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return df;
     }
 
-    public String getEntityFileName() {
-        return entityFileName;
+
+    /**
+     *
+     * @return entities set
+     */
+    public ImmutableSet<String> getEntitiesSet() {
+        return entitiesSet;
     }
 
-    public String getPredicateFileName() {
-        return predicateFileName;
+
+    /**
+     *
+     * @return predicate Set
+     */
+    public ImmutableSet<String> getPredicateSet() {
+        return predicateSet;
     }
+
 
     @Override
     protected void configure() {
--- test/edu/ufl/biotext2kb/extraction/extractentityandpredicate/ExtractEntityAndPredicateTest.java
+++ test/edu/ufl/biotext2kb/extraction/extractentityandpredicate/ExtractEntityAndPredicateTest.java
@@ -1,24 +1,97 @@
 package edu.ufl.biotext2kb.extraction.extractentityandpredicate;
 
-import org.junit.After;
+import com.google.common.collect.ImmutableSet;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.ufl.biotext2kb.BioText2KBUtils;
+import joinery.DataFrame;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class ExtractEntityAndPredicateTest {
     private ExtractEntityAndPredicate eep = null;
+    private static BufferedReader br;
+    private static ImmutableSet<String> sampleSents;
+
+    @BeforeClass
+    public static void loadTestSents(){
+        try {
+            File f = new File("data/Sentencesfrom23Abstracts.txt");
+            br = new BufferedReader(new FileReader("data/Sentencesfrom23Abstracts.txt"));
+            String line = null;
+            ImmutableSet.Builder<String> sentencesTextBuilder = ImmutableSet.<String>builder();
+
+            while((line = br.readLine()) != null){
+                sentencesTextBuilder.add(line);
+            }
+
+            sampleSents = sentencesTextBuilder.build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            BioText2KBUtils.closeStream(br);
+        }
+    }
 
     @Before
-    private void createExtractor(){
-        eep = new ExtractEntityAndPredicate();
+    public void createExtractor(){
+        eep = new ExtractEntityAndPredicate("data/EntityDict.csv", "data/PredicateDict.csv");
     }
 
     @Test
     public void testExtractFunc(){
-        String predFile = "";
-        String entFile = "";
+       
+    }
 
-        eep.setEntityFileName(entFile);
-        eep.setPredicateFileName(predFile);
+    @Test
+    public void testEncodingProblemInPubMedSents(){
+        HashMap<String, Integer> testMap = new HashMap<>();
+        int i = 0;
+        for(String each: sampleSents){
+            //System.out.println(each);
+            testMap.put(each, i++);
+        }
+
+        Assert.assertEquals(sampleSents.size(), testMap.size());
+    }
+
+    @Test
+    public void testWordTokenizer(){
+        for(String each: sampleSents){
+            
+        }
+    }
+
+    @Test
+    public void testReadInDict(){
+        System.out.println(eep.getEntitiesSet().size());
+        System.out.println(eep.getPredicateSet().size());
+    }
+
+    @Test
+    public void testReadCsv(){
+        String fileName = "data/EntityDict.csv";
+        File f = new File(fileName);
+        Assert.assertTrue(f.exists());
+        DataFrame<Object> df = null;
+        try {
+            df = DataFrame.readCsv(fileName);
+            System.out.print(df);
+            System.out.println(df.types());
+            System.out.println(df.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(df.get(1, "entity"), "breast");
     }
 
 }
